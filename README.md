0a6be6d26d0c73008f36e65f4727c133a22a94b9
Samuel Jansem <samuel.jansem@gmail.com>
2022-08-09 15:49:02 -0300

    add simplejwt package

---
+++ README.md
@@ -0,0 +1,5 @@
+References:
+
+https://docs.djangoproject.com/en/4.1/
+https://www.django-rest-framework.org/tutorial/quickstart/
+https://django-rest-framework-simplejwt.readthedocs.io/en/latest/getting_started.html
--- base/api/urls.py
+++ base/api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
-    path('', views.getRoutes)
+    path('', views.getRoutes),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
