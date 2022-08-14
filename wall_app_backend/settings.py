76294d19d9542012fa7425a82735f6e8c6f0bb7e
Samuel Jansem <samuel.jansem@gmail.com>
2022-08-14 11:27:34 -0300

    add environment variables

--- wall_app_backend/settings.py
+++ wall_app_backend/settings.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 from datetime import timedelta
+import environ
+
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
